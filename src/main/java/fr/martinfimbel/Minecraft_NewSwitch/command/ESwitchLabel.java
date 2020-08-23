cccd7b51013c7728e48e3570d95380f1f72bd338
Martin <lpourtou2@gmail.com>
2020-08-23 21:31:49 +0200

    Adding code linking to messages concerning switch commands

--- src/main/java/fr/martinfimbel/Minecraft_NewSwitch/command/ESwitchLabel.java
+++ src/main/java/fr/martinfimbel/Minecraft_NewSwitch/command/ESwitchLabel.java
@@ -3,8 +3,8 @@ package fr.martinfimbel.Minecraft_NewSwitch.command;
 import fr.pederobien.minecraftgameplateform.interfaces.element.ILabel;
 
 public enum ESwitchLabel implements ILabel {
-	PLAYER_DONT_REVIVE_TIME("PlayerDontReviveTime"), NUMBER_OF_SWITCHABLE_PLAYER_PER_TEAM("NumberOfSwitchablePlayerPerTeam"),
-	SWITCH_AFTER_BORDER_MOVES("SwitchAfterBorderMoves");
+	PLAYER_DONT_REVIVE_TIME("playerDontReviveTime"), NUMBER_OF_SWITCHABLE_PLAYER_PER_TEAM("numberOfSwitchablePlayerPerTeam"),
+	SWITCH_AFTER_BORDER_MOVES("switchAfterBorderMoves"), ONE_PLAYER_SWITCH("onePlayerSwitch"), START_SWITCH_TIME("startSwitchTime"), SWITCH_TIME("switchTime");
 
 	private String label;
 
