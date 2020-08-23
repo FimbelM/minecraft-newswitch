0ca00a386621ffebaca0f22a02bb998de6cb5d80
Martin <lpourtou2@gmail.com>
2020-08-23 21:29:40 +0200

    Adding command to setup the first switch time

---
+++ src/main/java/fr/martinfimbel/Minecraft_NewSwitch/command/StartSwitchTime.java
@@ -0,0 +1,43 @@
+package fr.martinfimbel.Minecraft_NewSwitch.command;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import fr.martinfimbel.Minecraft_NewSwitch.ESwitchMessageCode;
+import fr.martinfimbel.Minecraft_NewSwitch.interfaces.ISwitchConfiguration;
+import fr.pederobien.minecraftgameplateform.impl.editions.AbstractLabelEdition;
+
+public class StartSwitchTime extends AbstractLabelEdition<ISwitchConfiguration> {
+
+	public StartSwitchTime() {
+		super(ESwitchLabel.START_SWITCH_TIME, ESwitchMessageCode.START_SWITCH_TIME_EXPLANATION);
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		try {
+			get().setStartSwitchTime(LocalTime.parse(args[0]));
+			sendMessageToSender(sender, ESwitchMessageCode.START_SWITCH_TIME_DEFINED, "" + get().getStartSwitchTime().getHour(),
+					"" + get().getStartSwitchTime().getMinute(), "" + get().getStartSwitchTime().getSecond());
+			return true;
+		} catch (IndexOutOfBoundsException e) {
+			sendMessageToSender(sender, ESwitchMessageCode.SWITCH_TIME_MISSING_TIME);
+		} catch (DateTimeParseException e) {
+			sendMessageToSender(sender, ESwitchMessageCode.BAD_TIME_FORMAT);
+		}
+		return false;
+	}
+
+	@Override
+	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+		if (args.length == 1)
+			return Arrays.asList(getMessageFromDictionary(sender, ESwitchMessageCode.START_SWITCH_TIME_TAB_COMPLETE));
+		return emptyList();
+	}
+
+}
