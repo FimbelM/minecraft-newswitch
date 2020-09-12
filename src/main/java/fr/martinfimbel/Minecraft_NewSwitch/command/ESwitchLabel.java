0a3b6ae8677f2b9957b13687188373589f3ab56a
Martin <lpourtou2@gmail.com>
2020-09-12 19:58:40 +0200

    Adding Switch dependant labels

--- src/main/java/fr/martinfimbel/Minecraft_NewSwitch/command/ESwitchLabel.java
+++ src/main/java/fr/martinfimbel/Minecraft_NewSwitch/command/ESwitchLabel.java
@@ -4,7 +4,8 @@ import fr.pederobien.minecraftgameplateform.interfaces.element.ILabel;
 
 public enum ESwitchLabel implements ILabel {
 	PLAYER_DONT_REVIVE_TIME("playerDontReviveTime"), NUMBER_OF_SWITCHABLE_PLAYER_PER_TEAM("numberOfSwitchablePlayerPerTeam"),
-	SWITCH_AFTER_BORDER_MOVES("switchAfterBorderMoves"), ONE_PLAYER_SWITCH("onePlayerSwitch"), START_SWITCH_TIME("startSwitchTime"), SWITCH_TIME("switchTime");
+	SWITCH_AFTER_BORDER_MOVES("switchAfterBorderMoves"), ONE_PLAYER_SWITCH("onePlayerSwitch"), START_SWITCH_TIME("startSwitchTime"), SWITCH_TIME("switchTime"),
+	RANDOM_SWITCH("randomSwitch");
 
 	private String label;
 
