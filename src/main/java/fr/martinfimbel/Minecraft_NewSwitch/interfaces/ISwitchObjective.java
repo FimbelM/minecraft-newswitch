fcfd99c56eb24404897212ceabfe1cf520314151
Martin <lpourtou2@gmail.com>
2020-09-12 20:05:04 +0200

    Correcting grammar mistakes

--- src/main/java/fr/martinfimbel/Minecraft_NewSwitch/interfaces/ISwitchObjective.java
+++ src/main/java/fr/martinfimbel/Minecraft_NewSwitch/interfaces/ISwitchObjective.java
@@ -5,7 +5,7 @@ import fr.pederobien.minecraftgameplateform.interfaces.element.IGameObjective;
 public interface ISwitchObjective extends IGameObjective<ISwitchConfiguration> {
 
 	/**
-	 * Display not empty teams and the number of player in game mode Survival.
+	 * Display not empty teams and the number of players in game mode Survival.
 	 */
 	void addTeams();
 }
