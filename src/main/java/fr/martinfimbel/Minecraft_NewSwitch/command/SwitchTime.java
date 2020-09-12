6ecdf1db1591096e931028ea4badbe37b2b6531b
Martin <lpourtou2@gmail.com>
2020-09-12 20:00:59 +0200

    Modifying message sending method

--- src/main/java/fr/martinfimbel/Minecraft_NewSwitch/command/SwitchTime.java
+++ src/main/java/fr/martinfimbel/Minecraft_NewSwitch/command/SwitchTime.java
@@ -10,6 +10,7 @@ import org.bukkit.command.CommandSender;
 
 import fr.martinfimbel.Minecraft_NewSwitch.ESwitchMessageCode;
 import fr.martinfimbel.Minecraft_NewSwitch.interfaces.ISwitchConfiguration;
+import fr.pederobien.minecraftdevelopmenttoolkit.utils.DisplayHelper;
 import fr.pederobien.minecraftgameplateform.impl.editions.AbstractLabelEdition;
 
 public class SwitchTime extends AbstractLabelEdition<ISwitchConfiguration> {
@@ -22,8 +23,7 @@ public class SwitchTime extends AbstractLabelEdition<ISwitchConfiguration> {
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		try {
 			get().setPeriodSwitchTime(LocalTime.parse(args[0]));
-			sendMessageToSender(sender, ESwitchMessageCode.SWITCH_TIME_DEFINED, "" + get().getPeriodSwitchTime().getHour(), "" + get().getPeriodSwitchTime().getMinute(),
-					"" + get().getPeriodSwitchTime().getSecond());
+			sendMessageToSender(sender, ESwitchMessageCode.SWITCH_TIME_DEFINED, DisplayHelper.toString(get().getStartSwitchTime(), false));
 			return true;
 		} catch (IndexOutOfBoundsException e) {
 			sendMessageToSender(sender, ESwitchMessageCode.SWITCH_TIME_MISSING_TIME);
