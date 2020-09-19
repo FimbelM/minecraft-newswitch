467e39dfed42d07e160912cbc5a437bbf8b7ba53
Martin <lpourtou2@gmail.com>
2020-09-19 22:38:04 +0200

    Debugging bad behavior of method getBeforeAsCode

--- src/main/java/fr/martinfimbel/Minecraft_NewSwitch/entries/TimeBeforePVPEntry.java
+++ src/main/java/fr/martinfimbel/Minecraft_NewSwitch/entries/TimeBeforePVPEntry.java
@@ -12,6 +12,7 @@ import fr.pederobien.minecraftgameplateform.entries.simple.TimeTaskEntry;
 public class TimeBeforePVPEntry extends TimeTaskEntry {
 	private ISwitchConfiguration configuration;
 	private LocalTime timeBeforePvP;
+	private Boolean isPvpEnabled;
 
 	/**
 	 * Create an entry that displays the time remaining before PvP is activated or if it is, tells it
@@ -22,11 +23,13 @@ public class TimeBeforePVPEntry extends TimeTaskEntry {
 	public TimeBeforePVPEntry(int score, ISwitchConfiguration configuration) {
 		super(score);
 		this.configuration = configuration;
+		timeBeforePvP = configuration.getPvpTime();
+		isPvpEnabled = false;
 	}
 
 	@Override
 	public LocalTime getTime() {
-		return timeBeforePvP = configuration.getPvpTime().minusSeconds(getTask().getGameTime().toSecondOfDay());
+		return timeBeforePvP;
 	}
 
 	@Override
@@ -36,6 +39,22 @@ public class TimeBeforePVPEntry extends TimeTaskEntry {
 
 	@Override
 	public IMinecraftMessageCode getBeforeAsCode(Player player) {
-		return timeBeforePvP.toSecondOfDay() > 0 ? ESwitchMessageCode.ENTRY_PVP_DISABLED : ESwitchMessageCode.ENTRY_PVP_ENABLED;
+		if (timeBeforePvP.toSecondOfDay() > 0) {
+			timeBeforePvP = configuration.getPvpTime().minusSeconds(getTask().getGameTime().toSecondOfDay());
+			return timeBeforePvP.toSecondOfDay() > 0 ? ESwitchMessageCode.ENTRY_PVP_DISABLED : ESwitchMessageCode.ENTRY_PVP_ENABLED;
+		}
+		return ESwitchMessageCode.ENTRY_PVP_ENABLED;
 	}
+
+	@Override
+	protected boolean isBeforeConstant() {
+		if (timeBeforePvP.toSecondOfDay() > 0)
+			return true;
+		if (timeBeforePvP.toSecondOfDay() == 0 && !isPvpEnabled) {
+			isPvpEnabled = true;
+			return false;
+		}
+		return true;
+	}
+
 }
