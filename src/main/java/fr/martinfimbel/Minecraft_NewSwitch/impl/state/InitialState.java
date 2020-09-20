34854acd7d84c81f4e9e8ddf23fb928cfdffc406
Martin <lpourtou2@gmail.com>
2020-09-20 16:45:02 +0200

    Updating initial state with SwitchActivator class

--- src/main/java/fr/martinfimbel/Minecraft_NewSwitch/impl/state/InitialState.java
+++ src/main/java/fr/martinfimbel/Minecraft_NewSwitch/impl/state/InitialState.java
@@ -2,6 +2,7 @@ package fr.martinfimbel.Minecraft_NewSwitch.impl.state;
 
 import java.time.LocalTime;
 
+import fr.martinfimbel.Minecraft_NewSwitch.impl.SwitchActivator;
 import fr.martinfimbel.Minecraft_NewSwitch.interfaces.ISwitch;
 import fr.pederobien.minecraftgameplateform.interfaces.runtime.timeline.IObsTimeLine;
 import fr.pederobien.minecraftgameplateform.utils.Plateform;
@@ -11,10 +12,12 @@ import fr.pederobien.minecraftmanagers.WorldManager;
 
 public class InitialState extends AbstractState {
 	private IObsTimeLine teamObjective;
+	private SwitchActivator switchActivator;
 
 	public InitialState(ISwitch game) {
 		super(game);
 		teamObjective = new TeamObjective();
+		switchActivator = new SwitchActivator(getConfiguration());
 	}
 
 	@Override
@@ -22,6 +25,8 @@ public class InitialState extends AbstractState {
 		getConfiguration().getBorders().forEach(border -> border.apply(Plateform.getTimeLine()));
 		Plateform.getTimeLine().addObserver(getConfiguration().getPlayerDontReviveTime(), getGame());
 		Plateform.getTimeLine().addObserver(getConfiguration().getBorder(WorldManager.OVERWORLD).get().getStartTime(), teamObjective);
+		switchActivator.initialize();
+		Plateform.getTimeLine().addObserver(switchActivator.getNextNotifiedTime(), switchActivator);
 		PlayerManager.getPlayers().forEach(player -> getGame().createObjective(ScoreboardManager.createScoreboard(), player));
 		return true;
 	}
