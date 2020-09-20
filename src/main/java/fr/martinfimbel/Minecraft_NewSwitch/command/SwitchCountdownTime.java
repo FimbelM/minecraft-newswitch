480ced433297dca0b0afcb1eb57dc3ab7d08472f
Martin <lpourtou2@gmail.com>
2020-09-20 17:16:30 +0200

    Modifying the name of the method from Value to Time

---
+++ src/main/java/fr/martinfimbel/Minecraft_NewSwitch/command/SwitchCountdownTime.java
@@ -0,0 +1,43 @@
+package fr.martinfimbel.Minecraft_NewSwitch.command;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import fr.martinfimbel.Minecraft_NewSwitch.ESwitchMessageCode;
+import fr.martinfimbel.Minecraft_NewSwitch.interfaces.ISwitchConfiguration;
+import fr.pederobien.minecraftdevelopmenttoolkit.utils.DisplayHelper;
+import fr.pederobien.minecraftgameplateform.dictionary.ECommonMessageCode;
+import fr.pederobien.minecraftgameplateform.impl.editions.AbstractLabelEdition;
+
+public class SwitchCountdownTime extends AbstractLabelEdition<ISwitchConfiguration> {
+
+	public SwitchCountdownTime() {
+		super(ESwitchLabel.NUMBER_OF_SECONDS_COUNTDOWN, ESwitchMessageCode.NUMBER_OF_SECONDS_COUNTDOWN_EXPLANATION);
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		try {
+			get().setSwitchCountdownTime(LocalTime.parse(args[0]));
+			sendSynchro(sender, ESwitchMessageCode.NUMBER_OF_SECONDS_DEFINED, DisplayHelper.toString(get().getSwitchCountdownTime(), false));
+			return true;
+		} catch (IndexOutOfBoundsException e) {
+			sendSynchro(sender, ESwitchMessageCode.NUMBER_OF_SECONDS_MISSING_TIME);
+		} catch (DateTimeParseException e) {
+			sendSynchro(sender, ECommonMessageCode.COMMON_BAD_TIME_FORMAT);
+		}
+		return false;
+	}
+
+	@Override
+	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+		if (args.length == 1)
+			return Arrays.asList(getMessage(sender, ESwitchMessageCode.NUMBER_OF_SECONDS_COUNTDOWN_TAB_COMPLETE));
+		return emptyList();
+	}
+}
