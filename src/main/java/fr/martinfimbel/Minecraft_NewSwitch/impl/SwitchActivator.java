159260b061b644f7d1103f045e9fe0f2f75dd62b
Martin <lpourtou2@gmail.com>
2020-09-22 00:03:28 +0200

    Implementing getNextNotifiedTime method and upgrading switch time map generation

--- src/main/java/fr/martinfimbel/Minecraft_NewSwitch/impl/SwitchActivator.java
+++ src/main/java/fr/martinfimbel/Minecraft_NewSwitch/impl/SwitchActivator.java
@@ -1,9 +1,9 @@
 package fr.martinfimbel.Minecraft_NewSwitch.impl;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -13,32 +13,33 @@ import fr.martinfimbel.Minecraft_NewSwitch.interfaces.ISwitchConfiguration;
 import fr.pederobien.minecraftborder.interfaces.IBorderConfiguration;
 import fr.pederobien.minecraftgameplateform.interfaces.editions.IPlateformCodeSender;
 import fr.pederobien.minecraftgameplateform.interfaces.runtime.timeline.IObsTimeLine;
-import fr.pederobien.minecraftmanagers.BukkitManager;
 import fr.pederobien.minecraftmanagers.EColor;
+import fr.pederobien.minecraftmanagers.MessageManager;
 import fr.pederobien.minecraftmanagers.MessageManager.DisplayOption;
+import fr.pederobien.minecraftmanagers.MessageManager.TitleMessage;
+import fr.pederobien.minecraftmanagers.PlayerManager;
 import fr.pederobien.minecraftmanagers.WorldManager;
 
 public class SwitchActivator implements IObsTimeLine, IPlateformCodeSender {
 	private ISwitchConfiguration configuration;
 	private IBorderConfiguration borderConf;
-	private Integer countdownTime;
-	private Integer currentCountDown;
-	private Integer lambda;
-	private Boolean isRandomSwitchActivated;
+	private int countdownTime, currentCountdown, lambda, count;
+	private boolean isRandomSwitchActivated;
+	private Map<Integer, LocalTime> switchTimes;
 
 	public SwitchActivator(ISwitchConfiguration configuration) {
 		this.configuration = configuration;
 	}
 
 	public void initializeTimeMap() {
+		switchTimes = new HashMap<>();
 		borderConf = configuration.getBorder(WorldManager.OVERWORLD).get();
 		isRandomSwitchActivated = configuration.isRandomSwitchActivated();
 		countdownTime = configuration.getSwitchCountdownTime().toSecondOfDay();
+		currentCountdown = countdownTime;
 		lambda = configuration.getAverageNumberOfSwitch();
-		// TODO fill switch time map depending on is random or not
+		count = 0;
 
-		LinkedHashMap<Integer, LocalTime> switchTimes = new LinkedHashMap<>();
-		Map<Integer, LocalTime> sortedSwitchTimes = new LinkedHashMap<>();
 		LocalTime startSwitchTime = configuration.getStartSwitchTime();
 		LocalTime periodicSwitchTime = configuration.getPeriodSwitchTime();
 		LocalTime nextSwitchTime = startSwitchTime;
@@ -46,8 +47,11 @@ public class SwitchActivator implements IObsTimeLine, IPlateformCodeSender {
 		LocalTime lowerBound = configuration.getMinimalSwitchTime();
 		LocalTime tmax = configuration.getMaximalSwitchTime();
 		LocalTime minimalIntervalBetweenSwitches = configuration.getSwitchCountdownTime();
-		Boolean isSwitchAfterBorderMoves = configuration.isSwitchAfterBorderMovesActivated();
-		Integer switchCount = 0;
+		LocalTime randomGeneratedTime;
+		LocalTime upperBound;
+		boolean isSwitchAfterBorderMoves = configuration.isSwitchAfterBorderMovesActivated();
+		int randomGeneratedTimeInteger, switchCount = 0;
+
 		Random rand = new Random();
 
 		// Cas pas random
@@ -73,48 +77,46 @@ public class SwitchActivator implements IObsTimeLine, IPlateformCodeSender {
 				} while (nextSwitchTime.toSecondOfDay() < borderConf.getMoveTime().plusSeconds(borderStartTime.toSecondOfDay()).plusHours(1).toSecondOfDay());
 			}
 			// display map
-			BukkitManager.broadcastMessage("Switch times : ");
-			for (int count = 0; count < switchTimes.size(); count++) {
-				BukkitManager.broadcastMessage("count : " + count + " " + switchTimes.get(count).toString());
-			}
+			/*
+			 * BukkitManager.broadcastMessage("Switch times : "); for (int count = 0; count < switchTimes.size(); count++) {
+			 * BukkitManager.broadcastMessage("count : " + count + " " + switchTimes.get(count).toString()); }
+			 */
 		}
+
 		// cas random
 		else {
-			Integer randomGeneratedTimeInteger;
-			LocalTime randomGeneratedTime;
 
-			LocalTime upperBound;
 			// Define upper bound as a function of isSwitchafterBorderMoves true or false
 			if (!isSwitchAfterBorderMoves)
 				upperBound = borderStartTime.minusSeconds(tmax.toSecondOfDay());
 			else
-				upperBound = borderStartTime.plusSeconds(4000);
+				upperBound = borderStartTime.plusHours(1);
 
 			// Object that return random int.
 			switchCount = getPoissonRandom(lambda); // loi de poisson pour déterminer le nombre de switch en fonction d'un paramètre lambda
-			BukkitManager.broadcastMessage(switchCount.toString()); // debugging and information
-			switchTimes.put(0, LocalTime.of(0, 0, 0));
-			for (int i = 0; i < switchCount; i++) { // for each switchcount, associate a random time and then concatening it in the map
+			// BukkitManager.broadcastMessage("" + switchCount); // debugging and information
+			while (switchTimes.size() < switchCount) {
+
 				// generate a "integer" time between lower and upper bounds
 				randomGeneratedTimeInteger = rand.nextInt(upperBound.toSecondOfDay() - lowerBound.toSecondOfDay()) + lowerBound.toSecondOfDay();
 				randomGeneratedTime = LocalTime.of(0, 0, 0).plusSeconds(randomGeneratedTimeInteger);
-				BukkitManager.broadcastMessage("i : " + i + " generated time : " + randomGeneratedTime.toString());
-				// verify that the selected time isn't too close (less than the warning before switch time) from another already selected time
-				for (int j = 0; j < switchTimes.size(); j++) {
-					if (switchTimes.get(j).minusSeconds(randomGeneratedTimeInteger).toSecondOfDay() > minimalIntervalBetweenSwitches.toSecondOfDay()) {
-						switchTimes.put(i, randomGeneratedTime); // put the switch time value in the map
-					} else
-						break;
-				}
-			}
 
-			sortedSwitchTimes = sortMap(switchTimes);
+				// verify that the selected time isn't too close (less than the warning before switch time) from another already selected time
+				boolean isValidated = true;
+				for (int j = 0; j < switchTimes.size(); j++)
+					isValidated &= Math.abs(switchTimes.get(j).toSecondOfDay() - randomGeneratedTimeInteger) > minimalIntervalBetweenSwitches.toSecondOfDay();
+
+				// Put the time value in the map
+				if (isValidated)
+					switchTimes.put(switchTimes.size(), randomGeneratedTime);
+			}
+			sortMap();
 
 			// display map
-			for (Map.Entry<Integer, LocalTime> entry : sortedSwitchTimes.entrySet()) {
-				BukkitManager.broadcastMessage("Key after sort : " + entry.getKey());
-				BukkitManager.broadcastMessage("Value after sort : " + entry.getValue());
-			}
+			/*
+			 * for (Map.Entry<Integer, LocalTime> entry : switchTimes.entrySet()) { BukkitManager.broadcastMessage("times : " + entry.getKey()
+			 * + " - " + entry.getValue()); }
+			 */
 
 		}
 
@@ -127,28 +129,35 @@ public class SwitchActivator implements IObsTimeLine, IPlateformCodeSender {
 
 	@Override
 	public int getCurrentCountDown() {
-		return currentCountDown;
+		return currentCountdown;
 	}
 
 	@Override
 	public void onTime(LocalTime currentTime) {
-		// TODO do the switch (teams + tp)
+		// do the switch (teams + tp)
+		MessageManager.sendMessage(DisplayOption.TITLE, PlayerManager.getPlayer("Nightraven87"), TitleMessage.of("SWITCH", EColor.GOLD));
 
-		// Plateform.getOrCreateConfigurationHelper(configuration) --> accès à toutes les commandes concernant les équipes
+		// Plateform.getOrCreateConfigurationHelper(configuration) --> accès à toutes les commandes concernant les équipe;
+
+		// reset du countdown
+		currentCountdown = getCountDown();
 	}
 
 	@Override
 	public void onCountDownTime(LocalTime currentTime) {
-		sendNotSynchro(ESwitchMessageCode.NEXT_SWITCH_IN, DisplayOption.TITLE, EColor.GOLD, currentCountDown);
-		currentCountDown--;
+		sendNotSynchro(ESwitchMessageCode.NEXT_SWITCH_IN, DisplayOption.TITLE, EColor.GOLD, currentCountdown);
+		currentCountdown--;
 	}
 
 	@Override
 	public LocalTime getNextNotifiedTime() {
-		// recuperer les valeurs de la map
-		// comment faire???
+		if (count == switchTimes.size())
+			return LocalTime.of(0, 0, 0);
 
-		return LocalTime.of(0, 0, 35);
+		LocalTime nextSwitch = switchTimes.get(count);
+		count++;
+		return nextSwitch;
+
 	}
 
 	private static int getPoissonRandom(double mean) {
@@ -163,18 +172,19 @@ public class SwitchActivator implements IObsTimeLine, IPlateformCodeSender {
 		return k - 1;
 	}
 
-	private Map<Integer, LocalTime> sortMap(LinkedHashMap<Integer, LocalTime> map) {
-		List<Map.Entry<Integer, LocalTime>> switchTimesList = new LinkedList<>(map.entrySet());
+	private void sortMap() {
+		List<Map.Entry<Integer, LocalTime>> switchTimesList = new ArrayList<>(switchTimes.entrySet());
 
 		Collections.sort(switchTimesList, (o1, o2) -> o1.getValue().compareTo(o2.getValue()));
 
-		LinkedHashMap<Integer, LocalTime> result = new LinkedHashMap<>();
+		Map<Integer, LocalTime> result = new HashMap<>();
 		int counter = 0;
 		for (Map.Entry<Integer, LocalTime> entry : switchTimesList) {
 			result.put(counter, entry.getValue());
 			counter++;
 		}
-		return result;
+
+		switchTimes = result;
 	}
 
 	// rajouter classe interne qui permet de découper ontime en plusieurs méthodes
